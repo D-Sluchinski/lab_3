444dc7d387050484e5e2d9cc31412866e7302355
kellyokada <kelly.okada.p@gmail.com>
2025-02-06 17:20:05 -0700

    Commit 2

--- component/lab_3.tsx
+++ component/lab_3.tsx
@@ -1,33 +1,26 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Button, Text, StyleSheet } from 'react-native';
 import IncrementButton from '../component/increment';
 import DecrementButton from '../component/decrement';
 
-const Lab3: React.FC = () => {
-  const [counter, setCounter] = useState(0);
 
-  const increment = () => setCounter(prev => prev + 1);
-  const decrement = () => setCounter(prev => prev - 1);
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.counterText}>Counter: {counter}</Text>
-      <IncrementButton onIncrement={increment} />
-      <DecrementButton onDecrement={decrement} />
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  counterText: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
-});
+const Lab3 = () => {
+    const[ value, setValue ] = useState<number>(0);
+    return (
+        <View style={style.container}>
+            <Text style={style.text}>My Value: {value}</Text>
+            <View style={StyleSheet.subContainer}>
+                <Increment value={value} setValue={setValue}/>
+                <Decrement value={value} setValue={setValue}/>
+            </View>
+        </View>
+    )
+}
 
 export default Lab3;
+
+const stykes = StyleSheet.create({
+    container:{
+        
+    }
+})
