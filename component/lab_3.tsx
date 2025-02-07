772db659cf26a4df3eabd970bea889355e11a3c1
Pushpreet01 <166869140+Pushpreet01@users.noreply.github.com>
2025-02-06 21:00:59 -0700

    Update lab_3.tsx

--- component/lab_3.tsx
+++ component/lab_3.tsx
@@ -1,26 +1,36 @@
 import React, { useState } from 'react';
-import { Button, Text, StyleSheet } from 'react-native';
-import IncrementButton from '../component/increment';
-import DecrementButton from '../component/decrement';
-
-
+import {View, Text, StyleSheet } from 'react-native';
+import Increment from '../component/increment';
+import Decrement from '../component/decrement';
+ 
+ 
 const Lab3 = () => {
     const[ value, setValue ] = useState<number>(0);
     return (
-        <View style={style.container}>
-            <Text style={style.text}>My Value: {value}</Text>
-            <View style={StyleSheet.subContainer}>
+        <View style={styles.container}>
+            <Text style={styles.text}>My Value: {value}</Text>
+            <View style={styles.subContainer}>
                 <Increment value={value} setValue={setValue}/>
                 <Decrement value={value} setValue={setValue}/>
             </View>
         </View>
     )
 }
-
+ 
 export default Lab3;
-
-const stykes = StyleSheet.create({
-    container:{
-        
-    }
-})
+ 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    subContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '50%'
+    },
+    text: {
+        fontSize: 30,
+        marginBottom: 20}
+});
